d3d115ccef59aa5bd960db37ab8906efefb19732
Sam Tamlin <smtamlin@gmail.com>
2023-03-03 12:47:42 +0000

    Add connection to database and API doc draft

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-
+const db = require('./db/index.js')
 require('dotenv').config()
+
 
 const port = process.env.PORT
 
@@ -9,6 +10,19 @@ const port = process.env.PORT
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+
+app.get('/account/:id', (req, res, next) => {
+    db.query('SELECT * FROM user_account WHERE id = $1', [req.params.id], (err, result) => {
+      if (err) {
+        return next(err)
+      }
+      res.send(result.rows[0])
+    })
+  })
+
+
+
 
 app.listen(port, () => {
     console.log(`E-Commerce app listening on port ${port}`)
