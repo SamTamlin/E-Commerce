22e2448f0b5d94707a236c1c8322f05b03c8f036
Sam Tamlin <smtamlin@gmail.com>
2023-03-08 14:36:41 +0000

    Add local login, user endpoints and product endpoints

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const { Pool, Client } = require('pg')
+const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: process.env.PGUSER,
@@ -6,10 +6,188 @@ const pool = new Pool({
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT
-})
+});
+
+// LocalStratagy Helper
+const getUsernamePassword = (username, password, cb) => {
+    pool.query(
+        'SELECT username, password FROM user_account WHERE username = $1',
+        [username],
+        function(error, user) {
+            if(error) {return cb(error)}
+            if(!user) {return cb(null, false, {message: 'username does not exist'})}
+            if (password != user.rows[0].password) {
+                return cb(null, false, {message: 'incorrect password'})
+            }
+            return cb(null, user);
+        }
+    )
+};
+
+
+// user_account
+const getUser = (req, res) => {
+    const username = req.session.passport.user;
+    pool.query(
+        'SELECT * FROM user_account WHERE username = $1',
+        [username],
+        (error, results) => {
+            if(error) {
+                throw error
+            }
+            res.status(200).json(results.rows)
+        }
+    );
+};
+
+const createUser = (req, res) => {
+    const {username, email, first_name, last_name, password} = req.body;
+    pool.query(
+        'INSERT INTO user_account (username, email, first_name, last_name, password) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+        [username, email, first_name, last_name, password],
+        (error, results) => {
+            if(error) {
+                throw error
+            } else if (!Array.isArray(results.rows) || results.rows.length < 1) {
+                throw error
+            }
+            res.status(201).json(results.rows)
+        }
+    )
+};
+
+const updateUser = (req, res) => {
+    const username = req.session.passport.user;
+    const {email, first_name, last_name, password} = req.body;
+    pool.query(
+        'UPDATE user_account SET email = $1, first_name = $2, last_name = $3, password = $4 WHERE username = $5 RETURNING *',
+        [email, first_name, last_name, password, username],
+        (error, results) => {
+            if(error) {
+                throw error
+            }
+            if (typeof results.rows == 'undefined') {
+                res.status(404).send('Resource not found');
+            } else if (Array.isArray(results.rows) && results.rows.length < 1) {
+                res.status(404).send('User not found');
+            } else {
+                res.status(200).json(results.rows)
+            }
+        }
+    )
+};
+
+const deleteUser = (req, res) => {
+    const username = req.session.passport.user;
+    pool.query(
+        'DELETE FROM user_account WHERE username = $1',
+        [username],
+        (error, results) => {
+            if(error) {
+                throw error
+            }
+            res.status(204).send('No content')
+        }
+    )
+};
+
+// product
+const getProducts = (req, res) => {
+    pool.query(
+        'SELECT * FROM product',
+        (error, results) => {
+            if(error) {
+                throw error
+            }
+            res.status(200).json(results.rows)
+        }
+    )
+};
+
+const createProduct = (req, res) => {
+    const {name, price} = req.body;
+    pool.query(
+        // create product
+        'INSERT INTO product (name, price) VALUES ($1, $2) RETURNING *',
+        [name, price],
+        (error, results) => {
+            if(error) {
+                throw error
+            } else if (!Array.isArray(results.rows) || results.rows.length < 1) {
+                throw error
+            }
+            pool.query(
+                // return product with product_id
+                'SELECT * FROM product WHERE name = $1',
+                [name],
+                (error, results) => {
+                    if(error) {throw error}
+                    res.status(201).json(results.rows)
+                }
+            )
+        }
+    )
+};
+
+const getProduct = (req, res) => {
+    const {id} = req.params;
+    pool.query(
+        'SELECT * FROM product WHERE id = $1',
+        [id],
+        (error, results) => {
+            if(error) {throw error}
+            if(results.rows < 1) {
+                res.status(404).send(`Product with an id of ${id} does not exist.`)
+            } else {
+                res.status(200).json(results.rows)
+            }
+        }
+    )
+};
+
+const updateProduct = (req, res) => {
+    const {id} = req.params;
+    const {name, price} = req.body;
+    pool.query(
+        'UPDATE product SET name = $1, price = $2 WHERE id = $3 RETURNING *',
+        [name, price, id],
+        (error, results) => {
+            if(error) {throw error}
+            if (typeof results.rows == 'undefined') {
+                res.status(404).send('Resource not found');
+            } else if (Array.isArray(results.rows) && results.rows.length < 1) {
+                res.status(404).send('Product not found');
+            } else {
+                res.status(200).json(results.rows)
+            }
+        }
+    )
+};
+
+const deleteProduct = (req, res) => {
+    const {id} = req.params;
+    pool.query(
+        'DELETE FROM product WHERE id = $1',
+        [id],
+        (error, results) => {
+            if(error) {throw error}
+            res.status(204).send('No content')
+        }
+    )
+};
 
 module.exports = {
-    query: (text, params, callback) => {
-        return pool.query(text, params, callback)
-    },
-}
+    // Local Stratagy helper
+    getUsernamePassword,
+    // user_account
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    // product
+    getProducts,
+    createProduct,
+    getProduct,
+    updateProduct,
+    deleteProduct,
+};
--- index.js
+++ index.js
@@ -1,40 +1,89 @@
-const express = require('express')
-const app = express()
-const db = require('./db/index.js')
-require('dotenv').config()
-
+const express = require('express');
+const app = express();
+const db = require('./db/index.js');
+require('dotenv').config();
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 
 const port = process.env.PORT
+
+// Initialize Packages
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.post('/account/register', (req, res, next) => {
-  db.query('INSERT INTO user_account (email, first_name, last_name, password) VALUES($1, $2, $3, $4)',
-    [req.body.email, req.body.first_name, req.body.last_name, req.body.password],
-    (err, result) => {
-      if (err) {
-        return next(err)
-      }
-      res.status(200).send(req.body)
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { 
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000
+    }
   })
-})
+);
+app.use(passport.initialize());
+app.use(passport.session()); // Allows persistent logins
 
-app.get('/account/:id', (req, res, next) => {
-    db.query('SELECT * FROM user_account WHERE id = $1', [req.params.id], (err, result) => {
-      if (err) {
-        return next(err)
-      }
-      res.send(result.rows[0])
-    })
+// Passport local strategy
+passport.use(new LocalStrategy(
+  function verify(username, password, cb) {
+    db.getUsernamePassword(username, password, cb)}
+));
+
+// Stores username
+passport.serializeUser((user, done) => {
+  // console.log(`serailized user ${user.rows[0].username}`);
+  done(null, user.rows[0].username);
+});
+passport.deserializeUser((username, done) => {
+  // console.log(`deserialized user ${username}`);
+  done(null, username);
+});
+
+
+// ROUTES
+// user routes
+app.post('/account/register', db.createUser);
+
+app.post(
+  '/account/login', 
+  passport.authenticate('local', {
+    successRedirect: '/account/',
+    failureRedirect: '/account/login'
   })
+);
 
+app.post('/account/logout', (req, res, next) => {
+    req.logout((err) => {
+      if(err) {return next(err)}
+      res.redirect('/account/login');
+    });
+    
+  }
+);
+
+app.get('/account', db.getUser);
+app.put('/account', db.updateUser);
+app.delete('/account', db.deleteUser);
+
+// product routes
+app.get('/product', db.getProducts);
+app.post('/product', db.createProduct);
+app.get('/product/:id', db.getProduct);
+app.put('/product/:id', db.updateProduct);
+app.delete('/product/:id', db.deleteProduct);
+
+// order routes
+app.get('/account/order/:username');
+app.post('/account/order/:username');
+app.get('/account/order/:username/:orderId');
+app.put('/account/order/:username/:orderId');
+app.delete('/account/order/:username/:orderId');
 
 
 
 app.listen(port, () => {
     console.log(`E-Commerce app listening on port ${port}`)
-})
+});
